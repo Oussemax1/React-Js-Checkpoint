8d3dc4baba567bf471ffe5dd46d6310be0a6f299
Oussemax1 <amaroussema@gmail.com>
2020-12-15 15:47:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,56 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form class="needs-validation" novalidate>
+  <div class="form-row">
+    <div class="col-md-6 mb-3">
+      <label for="validationTooltip01">First name</label>
+      <input type="text" class="form-control" id="validationTooltip01"  required/>
+      <div class="invalid-tooltip">
+      Please put your First name.
+      </div>
     </div>
+    <div class="col-md-6 mb-3">
+      <label for="validationTooltip02">Last name</label>
+      <input type="text" class="form-control" id="validationTooltip02"  required/>
+      <div class="invalid-tooltip">
+      Please put your Last name.
+      </div>
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="col-md-6 mb-3">
+      <label for="validationTooltip03">City</label>
+      <input type="text" class="form-control" id="validationTooltip03" required/>
+      <div class="invalid-tooltip">
+        Please provide a valid city.
+      </div>
+    </div>
+    <div class="col-md-3 mb-3">
+      <label for="validationTooltip04">State</label>
+      <select class="custom-select" id="validationTooltip04" required>
+        <option selected disabled value=""> </option>
+        <option>Tunis</option>
+        <option>Sousse</option>
+        <option>Monastir</option>
+      </select>
+      <div class="invalid-tooltip">
+        Please select a valid state.
+      </div>
+    </div>
+    <div class="col-md-3 mb-3">
+      <label for="validationTooltip05">Zip</label>
+      <input type="text" class="form-control" id="validationTooltip05" required/>
+      <div class="invalid-tooltip">
+        Please provide a valid zip.
+      </div>
+    </div>
+  </div>
+  <button class="btn btn-primary" type="submit">Submit form</button>
+</form>
+      </header>
+   
   );
 }
 
